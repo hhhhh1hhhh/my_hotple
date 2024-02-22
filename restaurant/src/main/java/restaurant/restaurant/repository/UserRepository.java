f39f5b6863971f19d499a4e5e42133e2249d832a
hhhhh1hhhh <hhhhh_hhhh@naver.com>
2024-02-22 16:21:53 +0900

    feat: 로그인 검증 로직 구현

--- restaurant/src/main/java/restaurant/restaurant/repository/UserRepository.java
+++ restaurant/src/main/java/restaurant/restaurant/repository/UserRepository.java
@@ -6,4 +6,6 @@ import restaurant.restaurant.entity.UserEntity;
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 
     boolean existsByUsername(String username);
+
+    UserEntity findByUsername(String username);
 }
---
+++ restaurant/src/main/java/restaurant/restaurant/service/CustomUserDetailsService.java
@@ -0,0 +1,31 @@
+package restaurant.restaurant.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import restaurant.restaurant.dto.CustomUserDetails;
+import restaurant.restaurant.entity.UserEntity;
+import restaurant.restaurant.repository.UserRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
